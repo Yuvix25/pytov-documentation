6821cc31c11b968b850418d7c7ec8bb94484d1c0
Yuval Rosen <yuv.rosen@gmail.com>
2020-08-19 12:57:43 +0300

    change style

--- conf.py
+++ conf.py
@@ -99,4 +99,7 @@ import os
 os.system('pip3 install rtcat_sphinx_theme')
 import rtcat_sphinx_theme
 html_theme_path = [rtcat_sphinx_theme.get_html_theme_path()]
+html_css_files = [
+    'css/theme.css',
+]
 html_theme = 'rtcat_sphinx_theme'
